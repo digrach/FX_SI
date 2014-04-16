7e2b1f192d8e8fd97ca76403eb4c875320ce3352
unknown <colleyr@NOTEB-043738.chisholm.edu.au>
2014-04-16 12:41:19 +1000

    initial commit

---
+++ src/application/Main.java
@@ -0,0 +1,388 @@
+package application;
+
+import java.net.URL;
+import java.util.List;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+
+
+public class Main extends Application{// implements ChangeListener<Integer>, InvalidationListener {
+
+	Ut ut = new Ut();
+	FlowPane root;
+	Scene scene;
+	GraphicsContext gc;
+	Canvas canvas;
+	AnimationTimer timer;
+	int countdown = 50;
+
+	final URL shootSound = getClass().getResource("laser.mp3");
+	final AudioClip shootClip = new AudioClip(shootSound.toString());
+
+	final URL explosionSound = getClass().getResource("explosion.mp3");
+	final AudioClip explosionClip = new AudioClip(explosionSound.toString());
+
+
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			root = new FlowPane();
+			scene = new Scene(root,ut.getCanvasWidth(),ut.getCanvasHeight());
+			canvas = new Canvas();
+			canvas.setWidth(ut.getCanvasWidth());
+			canvas.setHeight(ut.getCanvasHeight());
+			gc = canvas.getGraphicsContext2D();
+			
+			drawAliens();
+			drawSpaceship();
+			addHandlers();
+			startTimer();
+
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				public void handle(WindowEvent we) {
+					System.out.println("Stage is closing");
+
+				}
+			});        
+
+			root.getChildren().add(canvas);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void restart() {
+		ut = new Ut();
+		drawAliens();
+		drawSpaceship();
+		addHandlers();
+		startTimer();
+	}
+
+	public void startTimer() {
+		timer = new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+
+				gc.setFill(Color.BLACK);
+				gc.fillRect(0, 0, ut.getCanvasWidth(), ut.getCanvasHeight());
+
+				ut.doGameLoop();
+				drawOnCanvas();
+
+				if (countdown == 0){
+					countdown = 100;
+					ut.dropProjectile();
+				}
+				countdown --;
+			}
+		};
+		timer.start();
+	}
+
+	public void drawOnCanvas() {
+		drawAliens();
+		drawSpaceship();
+		drawProjectiles();
+		drawAlienProjectiles();
+		drawScore();
+		drawMenu();
+	}
+
+	public void gameOver() {
+		timer.stop();
+		shootClip.stop();
+		explosionClip.stop();
+
+		final Stage dialog = new Stage();
+		dialog.initStyle(StageStyle.UNDECORATED);
+		dialog.setOpacity(.9);
+		dialog.initModality(Modality.WINDOW_MODAL);
+
+		VBox vb = new VBox();
+		vb.setPadding(new Insets(10));
+		vb.setSpacing(8);
+		vb.setAlignment(Pos.CENTER);
+
+		Text title = new Text("Game Over!");
+		title.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+		title.setFill(Color.CHARTREUSE);
+		vb.getChildren().add(title);
+
+		Label lblYourScoreLabel = new Label("Your score: " + ut.getScore());
+		lblYourScoreLabel.setFont(new Font("Arial", 40));
+		lblYourScoreLabel.setTextFill(Color.CHARTREUSE);
+
+		VBox.setMargin(lblYourScoreLabel, new Insets(0, 0, 0, 8));
+		vb.getChildren().add(lblYourScoreLabel);
+
+		
+
+		Button btnPlayAgain = new Button();
+		btnPlayAgain.setText("Play again");
+		btnPlayAgain.setFont(new Font("Arial", 25));
+
+		btnPlayAgain.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				restart();
+				dialog.close();
+			}
+		});
+
+		VBox.setMargin(lblYourScoreLabel, new Insets(0, 0, 0, 8));
+		vb.getChildren().add(btnPlayAgain);
+		
+		Button btnExit = new Button();
+		btnExit.setText("Exit");
+		btnExit.setFont(new Font("Arial", 25));
+
+		btnExit.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				Platform.exit();
+			}
+		});
+
+		VBox.setMargin(lblYourScoreLabel, new Insets(0, 0, 0, 8));
+		vb.getChildren().add(btnExit);
+
+		Scene dialogScene = new Scene(vb,ut.getCanvasWidth() / 3,ut.getCanvasHeight() / 3);
+		
+		// Capture space keypress as it is happening to prevent
+		// default behaviour.
+		dialogScene.addEventFilter( KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+		    @Override
+		    public void handle(KeyEvent event) {
+
+		        if (event.getEventType() == KeyEvent.KEY_PRESSED){
+		            // Consume Event before Bubbling Phase
+		            if ( event.getCode() == KeyCode.SPACE ){ 
+		                event.consume();
+		            }
+		        }
+		        }
+		    });
+		
+		dialogScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent arg0) {
+				if (arg0.getCode() == KeyCode.X) {
+					Platform.exit();
+				}
+				if (arg0.getCode() == KeyCode.P) {
+					restart();
+					dialog.close();
+				}
+			}
+			
+		});
+
+		dialogScene.setFill(Color.rgb(0, 0, 0, 1));
+		dialog.setScene(dialogScene);
+		dialog.show();
+	}
+
+	public void addHandlers() {
+
+		final BooleanProperty spacePressed = new SimpleBooleanProperty(false);
+		final BooleanProperty rightPressed = new SimpleBooleanProperty(false);
+		final BooleanProperty leftPressed = new SimpleBooleanProperty(false);
+		final BooleanBinding spaceAndRightPressed = spacePressed.and(rightPressed);
+		final BooleanBinding spaceAndLeftPressed = spacePressed.and(leftPressed);
+
+
+		ut.oScore.addListener(new ChangeListener<Number>(){
+			@Override
+			public void changed(ObservableValue<? extends Number> observable,
+					Number oldValue, Number newValue) {
+				explosionClip.play();				
+			}
+		});
+
+		ut.oGameOver.addListener(new ChangeListener<Boolean>(){
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable,
+					Boolean oldValue, Boolean newValue) {
+				if (newValue == true) {
+					ut.oGameOver.removeListener(this);
+					gameOver();				
+				}
+			}
+		});
+
+		rightPressed.addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable,
+					Boolean oldValue, Boolean newValue) {
+				if (newValue) {
+					ut.moveSpaceShip("east");
+				}
+			}
+		});
+
+
+		leftPressed.addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable,
+					Boolean oldValue, Boolean newValue) {
+				if (newValue) {
+					ut.moveSpaceShip("west");
+				}
+			}
+		});
+
+
+
+
+		spacePressed.addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable,
+					Boolean oldValue, Boolean newValue) {
+				if (newValue) {
+					ut.Shoot(System.currentTimeMillis());
+					shootClip.play();
+					System.out.println("SPACE");
+				}
+			}
+		});
+
+		spaceAndRightPressed.addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable,
+					Boolean oldValue, Boolean newValue) {
+				if (newValue) {
+					ut.moveSpaceShip("east");
+					ut.Shoot(System.currentTimeMillis());
+				}
+			}
+		});
+
+		spaceAndLeftPressed.addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable,
+					Boolean oldValue, Boolean newValue) {
+				if (newValue) {
+					ut.moveSpaceShip("west");
+					ut.Shoot(System.currentTimeMillis());
+				}
+			}
+		});
+
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent ke) {
+				if (ke.getCode() == KeyCode.SPACE) {
+					spacePressed.set(true);
+				} else if (ke.getCode() == KeyCode.RIGHT) {
+					rightPressed.set(true);
+				} else if (ke.getCode() == KeyCode.LEFT) {
+					leftPressed.set(true);
+				} else if (ke.getCode() == KeyCode.X) {
+					Platform.exit();
+				} 
+			}
+		});
+
+		scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent ke) {
+				if (ke.getCode() == KeyCode.SPACE) {
+					spacePressed.set(false);
+				} else if (ke.getCode() == KeyCode.RIGHT) {
+					rightPressed.set(false);
+				} else if (ke.getCode() == KeyCode.LEFT) {
+					leftPressed.set(false);
+				} 
+			}
+		});
+
+	}
+
+	public void drawAliens() {
+		gc.setFill(Color.CHARTREUSE);
+		Alien[] aliens = ut.getAlienFleet();
+		for (Alien a: aliens) {
+			if (a != null) {
+				gc.fillRect(a.getX(), a.getY(), a.getWidth(), a.getHeight());
+			}
+		}
+	}
+
+	public void drawSpaceship() {
+		gc.setFill(Color.WHITE);
+		SpaceShip s = ut.getSpaceship();
+		gc.fillRect(s.getX(), s.getY(), s.getWidth(), s.getHeight());
+	}
+
+	public void drawProjectiles() {
+		gc.setFill(Color.RED);
+		List<Projectile> projectiles = ut.getProjectiles();
+		for (Projectile p : projectiles) {
+			gc.fillRect(p.getX(), p.getY(), p.getWidth(), p.getHeight());
+		}
+	}
+
+	public void drawAlienProjectiles() {
+		gc.setFill(Color.CHARTREUSE);
+		List<Projectile> projectiles = ut.getFleetProjectiles();
+		for (Projectile p : projectiles) {
+			gc.fillRect(p.getX(), p.getY(), p.getWidth(), p.getHeight());
+		}
+	}
+
+	public void drawScore() {
+		gc.setFill(Color.FUCHSIA);
+		gc.fillText(Integer.toString(ut.getScore()), ut.getCanvasEndX() - 100, ut.getCanvasEndY() - 20);
+	}
+
+	public void drawMenu() {
+		gc.setFill(Color.ORANGE);
+		gc.fillText("Exit = x", ut.getCanvasStartX() + 100, ut.getCanvasEndY() - 20);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+
+
+
+
+
+}
---
+++ src/application/Ut.java
@@ -0,0 +1,656 @@
+package application;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableIntegerValue;
+
+
+
+
+public class Ut {
+
+	private double canvasWidth;
+	private double canvasHeight;
+	private double buffer;
+	private double widthDivisor;
+	private double heightPercentage;
+	private double fleetTravelRate;
+	private double spaceshipTravelRate;
+	private double spaceshipProjectileTravelRate;
+	private double fleetProjectileTravelRate;
+	private int numOfAliens;
+	private int numOfAssetsOnRow;
+
+	// changable
+	private String spaceshipDirection;
+	private String fleetDirection;
+	private int score;
+	IntegerProperty oScore = new SimpleIntegerProperty();
+	BooleanProperty oGameOver = new SimpleBooleanProperty();
+	private int finishScore;
+	private long lastShot;
+
+	// Calculated
+	private double canvasStartX;
+	private double canvasStartY;
+	private double canvasEndX;
+	private double canvasEndY;
+	private double spaceShipTravelRate;
+	private double gameAssetWidth;
+	private double gameAssetHeight;
+	private double assetSpacerWidth;
+	private double assetSpacerHeight;
+
+	private Alien[] alienFleet; 
+	private SpaceShip spaceship;
+	private List<Projectile> spceshipProjectiles;
+	private List<Projectile> fleetProjectiles;
+	
+	
+	public Ut() {
+		initVars();
+	}
+
+	public void initVars() {
+		spceshipProjectiles = new ArrayList<Projectile>();
+		fleetProjectiles = new ArrayList<Projectile>();
+		canvasWidth = 1000;
+		canvasHeight = 500;
+		buffer = 0;
+		widthDivisor = 20;
+		heightPercentage = 0.75;
+		spaceshipDirection = "east";
+		fleetDirection = "east";
+		fleetTravelRate = 0.001;
+		spaceshipTravelRate = 0.01;
+		spaceshipProjectileTravelRate = 0.03;
+		fleetProjectileTravelRate = 0.003;
+		numOfAliens = 15;
+		numOfAssetsOnRow = 5;
+		finishScore = numOfAliens;
+		makeCanvasDimensions();
+		makeGameAssetDimensions();
+		initialiseSpaceshipPosition();
+		makeFleetStartX();
+		makeFleetStartY();
+		initialiseAlienFleetPosition();		
+	}
+
+	
+	public void doGameLoop() {
+		updateAlienFleetPosition();
+		updateProjectilePostion();
+		updateAlienProjectilePostion();
+		detectAlienCollisionWithSpaceship();
+		detectProjectileCollisionWithSpaceship();
+	}
+	
+	public SpaceShip initialiseSpaceshipPosition() {
+		double middleCoordinate = makeMiddleCoordinate();
+		double x = middleCoordinate - (gameAssetWidth / 2);
+		double y = canvasEndY - (gameAssetHeight * 2);
+		spaceship = new SpaceShip(x,y,gameAssetWidth, gameAssetHeight);
+		return spaceship;
+	}
+	
+	public boolean detectAlienCollisionWithSpaceship() {
+		for (Alien a: alienFleet) {
+			if (a != null) {
+				if (a.getX() + a.getWidth() >= spaceship.getX() 
+						&& a.getX() <= spaceship.getX() +  spaceship.getWidth()
+						&& a.getY() + a.getHeight() >= spaceship.getY()
+						&& a.getY() <= spaceship.getY() + spaceship.getHeight()) {
+					oGameOver.set(true);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean detectProjectileCollisionWithAlien(Alien a) {
+		Iterator<Projectile> itr = spceshipProjectiles.iterator();
+		while(itr.hasNext()) {
+			Projectile p = itr.next();
+			if (p.getX() + p.getWidth() >= a.getX() 
+					&& p.getX() <= a.getX() + a.getWidth()
+					&&
+					p.getY() + p.getHeight() >= a.getY() && p.getY() <= a.getY() + a.getHeight()) {
+				System.out.println("COLLISION");
+				itr.remove();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean detectProjectileCollisionWithSpaceship() {
+		Iterator<Projectile> itr = fleetProjectiles.iterator();
+		while(itr.hasNext()) {
+			Projectile p = itr.next();
+			if (p.getX() + p.getWidth() >= spaceship.getX() 
+					&& p.getX() <= spaceship.getX() + spaceship.getWidth()
+					&&
+					p.getY() + p.getHeight() >= spaceship.getY() && p.getY() <= spaceship.getY() + spaceship.getHeight()) {
+				itr.remove();
+				oGameOver.set(true);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void dropProjectile() {
+		int numOfCols = numOfAssetsOnRow - 1;
+		Alien ra = null;
+		int index = 0;
+		int randomCol = (int) (Math.random() * (numOfCols + 1));
+		index = (10 + randomCol);
+		while (ra == null && index >= 0) {
+			ra = alienFleet[index];
+			index -= 5;
+		}
+		if (ra != null) {
+			fleetProjectiles.add(new Projectile(ra.getX() + (gameAssetWidth / 2), ra.getY() + (gameAssetHeight + 1),3,3));
+		}
+	}
+	
+	public void updateAlienFleetPosition() {
+		double axisLength = makeAxisLength(canvasStartX, canvasEndX);
+		double travelIncrement = fleetTravelRate * axisLength;
+		if (fleetDirection.equals("east")) {
+			double mostEast = 0;
+			for (Alien a : alienFleet) {
+				if (a != null) {
+					if (a.getX() > mostEast) {
+						mostEast = a.getX();
+					}
+				}
+			}
+			double distanceEast = calculateDistanceEast(mostEast);
+			if ((mostEast + gameAssetWidth) + travelIncrement > (canvasEndX)) {
+				travelIncrement = distanceEast - gameAssetWidth;
+				fleetDirection = "west";
+				moveFleetSouth();
+			}
+			moveFleetEast(travelIncrement);
+		}
+		if (fleetDirection.equals("west")) {
+			double mostWest = canvasEndX;
+			for (Alien a : alienFleet) {
+				if (a != null) {
+					if (a.getX() < mostWest) {
+						mostWest = a.getX();
+					}
+				}
+			}
+			double distanceWest = calculateDistanceWest(mostWest);
+			if (mostWest - travelIncrement < canvasStartX) {
+				travelIncrement = distanceWest;
+				fleetDirection = "east";
+				moveFleetSouth();
+			}
+			moveFleetWest(travelIncrement);
+		}
+	}
+
+	public void updateProjectilePostion() {
+		double axisLength = makeAxisLength(canvasStartY, canvasEndY);
+		double travelIncrement = spaceshipProjectileTravelRate * axisLength;
+		Iterator<Projectile> itr = spceshipProjectiles.iterator();
+		while(itr.hasNext()) {
+			Projectile p = itr.next();
+			double projectileY = p.getY();
+			if (projectileY - travelIncrement < canvasStartY) {
+				itr.remove();
+			} else {
+				p.setY(projectileY - travelIncrement);
+			}
+		}
+	}
+
+	public void fireProjectile(double sourceX, double sourceY) {
+		spceshipProjectiles.add(new Projectile(sourceX, sourceY + 1, 3, 3));
+	}
+
+	public void updateAlienProjectilePostion() {
+		double axisLength = makeAxisLength(canvasStartY, canvasEndY);
+		double travelIncrement = fleetProjectileTravelRate * axisLength;
+		Iterator<Projectile> itr = fleetProjectiles.iterator();
+		while(itr.hasNext()) {
+			Projectile p = itr.next();
+			double projectileY = p.getY();
+			if (projectileY + travelIncrement > canvasEndY) {
+				itr.remove();
+			} else {
+				p.setY(projectileY + travelIncrement);
+			}
+		}
+	}
+	
+	public void initialiseAlienFleetPosition() {
+		alienFleet = new Alien[numOfAliens];
+		assetSpacerHeight = gameAssetHeight;
+		int alienCount = 0;
+		int aliensOnRowCount = 0;
+		double currentX = makeFleetStartX();
+		double currentY = makeFleetStartY();
+		while (alienCount < numOfAliens) {
+			Alien a = new Alien(currentX, currentY, gameAssetWidth, gameAssetHeight);
+			alienFleet[alienCount] = a;
+			currentX += gameAssetWidth + assetSpacerWidth;
+			aliensOnRowCount ++;
+			alienCount ++;
+			if (aliensOnRowCount > numOfAssetsOnRow - 1) {
+				aliensOnRowCount = 0;
+				currentX = makeFleetStartX();
+				currentY += assetSpacerHeight * 2;
+			}
+		}
+	}
+	
+	public void updateSpaceshipPosition(String direction) {
+		double axisLength = makeAxisLength(canvasStartX, canvasEndX);
+		double travelIncrement = spaceshipTravelRate * axisLength;
+		double updatedX = 0;
+		if (direction.equals("west")) {
+			updatedX = spaceship.getX() - travelIncrement;
+			if (checkForWestEdge(updatedX)) {
+				spaceship.setX(canvasStartX);
+				spaceshipDirection = "east";
+			} else {
+				spaceship.setX(updatedX);
+			}
+		}
+		if (direction.equals("east")) {
+			updatedX = spaceship.getX() + travelIncrement;
+			if (checkForEastEdge(updatedX, spaceship.getWidth())) {
+				spaceship.setX(canvasEndX - spaceship.getWidth());
+				spaceshipDirection = "west";
+			} else {
+				spaceship.setX(updatedX);
+			}
+		}
+	}
+
+	public void makeCanvasDimensions() {
+		canvasStartX = buffer;
+		canvasStartY = buffer;
+		canvasEndX = canvasWidth - buffer;
+		canvasEndY = canvasHeight - buffer;
+	}
+
+	public double makeAxisLength(double startCoord, double endCoord) {
+		double axisLength = endCoord - startCoord;
+		return axisLength;
+	}
+
+	public void makeGameAssetDimensions() {
+		gameAssetWidth = canvasWidth / widthDivisor;
+		gameAssetHeight = heightPercentage * gameAssetWidth;
+	}
+
+	public double makeMiddleCoordinate() {
+		double middleCoordinate = (canvasEndX - canvasStartX) / 2;
+		return middleCoordinate;
+	}
+
+	public double makeFleetStartX() {
+		double axisLength = makeAxisLength(canvasStartX, canvasEndX);
+		double remainingWidthOnX = axisLength - (gameAssetWidth * numOfAssetsOnRow);
+		assetSpacerWidth = remainingWidthOnX / (numOfAssetsOnRow + 1);
+		double fleetStartX = assetSpacerWidth;
+		return fleetStartX;
+	}
+
+	public double makeFleetStartY() {
+		double fleetStartY = gameAssetHeight;
+		return fleetStartY;
+	}
+
+	public double calculateDistanceWest(double positionOnAxis) {
+		double distanceWest = positionOnAxis - canvasStartX;
+		return distanceWest;
+	}
+
+	public double calculateDistanceEast(double positionOnAxis) {
+		double axisLength = makeAxisLength(canvasStartX, canvasEndX);
+		double distanceEast = axisLength - positionOnAxis;
+		return distanceEast;
+	}
+
+	public double calculateDistanceNorth(double positionOnAxis) {
+		double distanceNorth = positionOnAxis - canvasStartY;
+		return distanceNorth;
+	}
+
+	public boolean checkForWestEdge(double xCoord) {
+		boolean detected = false;
+		if (xCoord <= canvasStartX) {
+			detected = true;
+		}
+		return detected;
+	}
+
+	public boolean checkForEastEdge(double xCoord, double widthToCheck) {
+		boolean detected = false;
+		if (xCoord  + widthToCheck >= canvasEndX) {
+			detected = true;
+		}
+		return detected;
+	}
+
+	public void moveFleetEast(double travelIncrement) {
+		for (int x = 0; x < alienFleet.length; x++) {
+			if (alienFleet[x] != null) {
+				double updatedX = alienFleet[x].getX() + travelIncrement;
+				alienFleet[x].setX(updatedX);
+				if (detectProjectileCollisionWithAlien(alienFleet[x])) {
+					alienFleet[x] = null;
+//					score ++;
+//					oScore.setValue(score);
+					updateScore();
+				}
+			}
+		}
+	}
+
+	public void moveFleetWest(double travelIncrement) {
+		for (int x = 0; x < alienFleet.length; x++) {
+			if (alienFleet[x] != null) {
+				double updatedX = alienFleet[x].getX() - travelIncrement;
+				alienFleet[x].setX(updatedX);
+				if (detectProjectileCollisionWithAlien(alienFleet[x])) {
+					alienFleet[x] = null;
+//					score ++;
+//					oScore.setValue(score);
+					updateScore();
+				}
+			}
+		}
+	}
+
+	public void moveFleetSouth() {
+		for (int x = 0; x < alienFleet.length; x++) {
+			if (alienFleet[x] != null) {
+				double updatedY = alienFleet[x].getY() + (gameAssetHeight / 2);
+				alienFleet[x].setY(updatedY);
+				if (detectProjectileCollisionWithAlien(alienFleet[x])) {
+					alienFleet[x] = null;
+//					score ++;
+//					oScore.setValue(score);
+					updateScore();
+				}
+			}
+		}
+	}
+
+	public void updateScore() {
+		score ++;
+		oScore.setValue(score);
+		if(score == finishScore) {
+			oGameOver.set(true);
+		}
+	}
+
+	// For use by UI
+	public void moveSpaceShip(String direction) {
+		updateSpaceshipPosition(direction);
+	}
+
+	public void Shoot(Long now) {
+		if(now > lastShot + 300) {
+			fireProjectile(spaceship.getX() + (gameAssetWidth / 2),spaceship.getY());
+			lastShot = now;
+		}
+	}
+
+	
+
+	// Get / set
+	public Alien[] getAlienFleet() {
+		return alienFleet;
+	}
+
+	public double getGameAssetWidth() {
+		return gameAssetWidth;
+	}
+
+	public double getGameAssetHeight() {
+		return gameAssetHeight;
+	}
+
+	public double getCanvasWidth() {
+		return canvasWidth;
+	}
+
+	public double getCanvasHeight() {
+		return canvasHeight;
+	}
+
+	public double getBuffer() {
+		return buffer;
+	}
+
+	public double getWidthDivisor() {
+		return widthDivisor;
+	}
+
+	public double getHeightPercentage() {
+		return heightPercentage;
+	}
+
+	public double getFleetTravelRate() {
+		return fleetTravelRate;
+	}
+
+	public double getSpaceshipTravelRate() {
+		return spaceshipTravelRate;
+	}
+
+	public double getProjectileTravelRate() {
+		return spaceshipProjectileTravelRate;
+	}
+
+	public int getNumOfAliens() {
+		return numOfAliens;
+	}
+
+	public int getNumOfAssetsOnRow() {
+		return numOfAssetsOnRow;
+	}
+
+	public String getSpaceshipDirection() {
+		return spaceshipDirection;
+	}
+
+	public String getFleetDirection() {
+		return fleetDirection;
+	}
+
+	public double getCanvasStartX() {
+		return canvasStartX;
+	}
+
+	public double getCanvasStartY() {
+		return canvasStartY;
+	}
+
+	public double getCanvasEndX() {
+		return canvasEndX;
+	}
+
+	public double getCanvasEndY() {
+		return canvasEndY;
+	}
+
+	public double getSpaceShipTravelRate() {
+		return spaceShipTravelRate;
+	}
+
+	public double getAssetSpacerWidth() {
+		return assetSpacerWidth;
+	}
+
+	public double getAssetSpacerHeight() {
+		return assetSpacerHeight;
+	}
+
+	public SpaceShip getSpaceship() {
+		return spaceship;
+	}
+
+	public List<Projectile> getProjectiles() {
+		return spceshipProjectiles;
+	}
+
+	public void setCanvasWidth(double canvasWidth) {
+		this.canvasWidth = canvasWidth;
+	}
+
+	public void setCanvasHeight(double canvasHeight) {
+		this.canvasHeight = canvasHeight;
+	}
+
+	public void setBuffer(double buffer) {
+		this.buffer = buffer;
+	}
+
+	public void setWidthDivisor(double widthDivisor) {
+		this.widthDivisor = widthDivisor;
+	}
+
+	public void setHeightPercentage(double heightPercentage) {
+		this.heightPercentage = heightPercentage;
+	}
+
+	public void setFleetTravelRate(double fleetTravelRate) {
+		this.fleetTravelRate = fleetTravelRate;
+	}
+
+	public void setSpaceshipTravelRate(double spaceshipTravelRate) {
+		this.spaceshipTravelRate = spaceshipTravelRate;
+	}
+
+	public void setProjectileTravelRate(double projectileTravelRate) {
+		this.spaceshipProjectileTravelRate = projectileTravelRate;
+	}
+
+	public void setNumOfAliens(int numOfAliens) {
+		this.numOfAliens = numOfAliens;
+	}
+
+	public void setNumOfAssetsOnRow(int numOfAssetsOnRow) {
+		this.numOfAssetsOnRow = numOfAssetsOnRow;
+	}
+
+	public void setSpaceshipDirection(String spaceshipDirection) {
+		this.spaceshipDirection = spaceshipDirection;
+	}
+
+	public void setFleetDirection(String fleetDirection) {
+		this.fleetDirection = fleetDirection;
+	}
+
+	public void setCanvasStartX(double canvasStartX) {
+		this.canvasStartX = canvasStartX;
+	}
+
+	public void setCanvasStartY(double canvasStartY) {
+		this.canvasStartY = canvasStartY;
+	}
+
+	public void setCanvasEndX(double canvasEndX) {
+		this.canvasEndX = canvasEndX;
+	}
+
+	public void setCanvasEndY(double canvasEndY) {
+		this.canvasEndY = canvasEndY;
+	}
+
+	public void setSpaceShipTravelRate(double spaceShipTravelRate) {
+		this.spaceShipTravelRate = spaceShipTravelRate;
+	}
+
+	public void setGameAssetWidth(double gameAssetWidth) {
+		this.gameAssetWidth = gameAssetWidth;
+	}
+
+	public void setGameAssetHeight(double gameAssetHeight) {
+		this.gameAssetHeight = gameAssetHeight;
+	}
+
+	public void setAssetSpacerWidth(double assetSpacerWidth) {
+		this.assetSpacerWidth = assetSpacerWidth;
+	}
+
+	public void setAssetSpacerHeight(double assetSpacerHeight) {
+		this.assetSpacerHeight = assetSpacerHeight;
+	}
+
+	public void setAlienFleet(Alien[] alienFleet) {
+		this.alienFleet = alienFleet;
+	}
+
+	public void setSpaceship(SpaceShip spaceship) {
+		this.spaceship = spaceship;
+	}
+
+	public void setProjectiles(List<Projectile> projectiles) {
+		this.spceshipProjectiles = projectiles;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public int getFinishScore() {
+		return finishScore;
+	}
+
+	public void setFinishScore(int finishScore) {
+		this.finishScore = finishScore;
+	}
+
+	public List<Projectile> getSpceshipProjectiles() {
+		return spceshipProjectiles;
+	}
+
+	public void setSpceshipProjectiles(List<Projectile> spceshipProjectiles) {
+		this.spceshipProjectiles = spceshipProjectiles;
+	}
+
+	public List<Projectile> getFleetProjectiles() {
+		return fleetProjectiles;
+	}
+
+	public void setFleetProjectiles(List<Projectile> fleetProjectiles) {
+		this.fleetProjectiles = fleetProjectiles;
+	}
+
+	
+	
+
+	
+
+
+
+
+
+
+
+}
+
+
+
